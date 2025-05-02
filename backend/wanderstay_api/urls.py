8fe710e2d88cf4af28632f43c9c90068cdabd285
naveedhsn <naveedhossaiin@gmail.com>
2025-05-03 01:47:04 +0600

    update backend

--- backend/wanderstay_api/urls.py
+++ backend/wanderstay_api/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include, re_path
 from rest_framework import routers
 from blog.views import PostViewSet, CategoryViewSet, SubscribeView
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve as static_serve  # ✅ This fixes the NameError
+
 # API router
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -14,18 +18,12 @@ urlpatterns = [
     path('api/subscribe/', SubscribeView.as_view(), name='subscribe'),
 ]
 
-# Serve media files only in development
-from django.conf import settings
-from django.conf.urls.static import static
-
+# Serve media files correctly based on environment
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 else:
-    # Serve media files in production using re_path
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', static_serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
     ]
-
