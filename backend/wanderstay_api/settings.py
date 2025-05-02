6b8f19d6f03eb446c927e7c0b1b871fcca17e6ba
naveedhsn <naveedhossaiin@gmail.com>
2025-05-02 19:17:00 +0600

    backend update

--- backend/wanderstay_api/settings.py
+++ backend/wanderstay_api/settings.py
@@ -50,12 +50,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wanderstay_api.wsgi.application'
 
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
